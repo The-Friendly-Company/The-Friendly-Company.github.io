4ed4bc4802094cc05126d33cc72bee68cd3a4ecc
Levine-Sam <sl10015@nyu.edu>
2024-04-06 23:19:14 -0400

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,31 @@
+import { Inter } from 'next/font/google'
+import clsx from 'clsx'
+
+import '@/styles/tailwind.css'
+import { type Metadata } from 'next'
+
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-inter',
+})
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s - Friendly',
+    default: 'Friendly - The place to find a friend',
+  },
+  description: 'The place where anyone can find truely meaningful connections',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en" className={clsx('bg-gray-50 antialiased', inter.variable)}>
+      <body>{children}</body>
+    </html>
+  )
+}
